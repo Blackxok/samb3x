2b04ef8b4a6263d7ec55c7ef359f2e6f449b0a44
Blackxok <nasridoni1@gmail.com>
2024-12-15 18:28:59 +0500

    tag added

---
+++ src/components/text/text.tsx
@@ -0,0 +1,17 @@
+import cn from 'classnames'
+import style from './text.module.css'
+import { TextProps } from './text.props'
+
+export default function text({ size = 'm', children }: TextProps): JSX.Element {
+	return (
+		<p
+			className={cn(style.p, {
+				[style.s]: size === 's',
+				[style.m]: size === 'm',
+				[style.l]: size === 'l',
+			})}
+		>
+			{children}
+		</p>
+	)
+}
