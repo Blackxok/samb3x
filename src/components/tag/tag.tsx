7bc3c2a3367ec47eadf0c2c69d8337aece588a60
Blackxok <nasridoni1@gmail.com>
2024-12-15 18:29:53 +0500

    tag component added

---
+++ src/components/tag/tag.tsx
@@ -0,0 +1,19 @@
+import cn from 'classnames'
+import style from './tag.module.css'
+import { TagProps } from './tag.props'
+
+export default function Tag({ size = 'm', color = 'pr', children }: TagProps): JSX.Element {
+	return (
+		<div
+			className={cn(style.tag, {
+				[style.s]: size === 's',
+				[style.m]: size === 'm',
+				[style.pr]: color === 'pr',
+				[style.gr]: color === 'gr',
+				[style.re]: color === 're',
+			})}
+		>
+			{children}
+		</div>
+	)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { Heading } from '../components'
+import { Heading, Tag, Text } from '../components'
 
 export default function Home() {
 	return (
 		<main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
 			<Heading tag='h1'>Assalomu alekum</Heading>
+			<Text>somethign</Text>
+			<Tag color='re'>some</Tag>
+			<Tag color='gr'>some</Tag>
+			<Tag color='pr'>some</Tag>
 		</main>
 	)
 }
